8126e1cb68d89fcf96081f70e81cb5d5b92cbf13
cfm75 <cfm75@georgetown.edu>
2024-02-27 12:11:02 -0500

    New additions from Greg

--- Disease Network Code.R
+++ Disease Network Code.R
@@ -1,85 +1,115 @@
-library(tidyverse)
+library(tidyverse); library(fs)
 
-#Specify parameters 
-#Vary some parameters (# of indivuduals or connectedness)
-#Need a break, if this isn't going to work, skip it (if some to infect = 0, then skip time step)
-#see where it gets stuck, if ToInfect, (sum=0, __)
+dir_create("Outputs")
 
+# Specify parameters
 
 NID <- 100
 
-TransmissionProbability <- 0.25
+TransmissionProbability <- 0.05
 
-
-#for(i in 1:100){
-
-t <- 0
-
-#Run the initial loop
-
-Population<- data.frame(ID= 1:NID, TimeStep = NA)
-
-Population$I <- 0
-
-#Random number selection/generation of n group 
-
-(PatientZero <- sample(1:NID, 1, replace=T))
-
-Population[PatientZero,]
-
-#This individual is now infected
-Population[PatientZero, "I"] <- 1
-Population[PatientZero, "TimeStep"] <- t
-
-(Matrix <- matrix(0, ncol = NID, nrow = NID))
-
-#~5% of them would be full
-Connections <- sample(1: (NID*NID), NID*NID/20)
-
-Matrix[Connections] <- 1
-
-Matrix[PatientZero,]
-
-t <-1 
-
-ToInfect <- rbinom(NID, 1, TransmissionProbability)
-
-ToInfect <- ToInfect*Matrix[PatientZero,]
-
-WhichtoInfect <- which(ToInfect == 1)
-
-Population[WhichtoInfect, "I"] <- 1
-Population[WhichtoInfect, "TimeStep"] <- t
-
-# Population %>% filter(TimeStep== 0)
-
-#Defining infected
-
-for(t in 2:100){
-  print(t)
-  Infected <- which(Population$I ==1)
+for(i in 1:100){
   
-  Matrix[Infected,] 
+  print(i)
   
-  ToInfect <- Matrix[Infected,] %>% colSums
+  # Run the initial loop
   
+  t <- 0
   
-  ToInfect[ToInfect>1] <- 1
-  ToInfect <- ToInfect*rbinom(NID, 1, TransmissionProbability)
+  Population <- data.frame(ID = 1:NID,
+                           TimeStep = NA)
   
-  WhichtoInfect <- which(ToInfect == 1)
-  WhichtoInfect <- WhichtoInfect %>% setdiff(which(Population$I == 1))
+  Population$I <- 0
   
-  Population[WhichtoInfect, "I"] <- 1
-  Population[WhichtoInfect, "TimeStep"] <- t
+  (PatientZero <- 
+      sample(1:NID, 1, replace = T))
+  
+  Population[PatientZero,]
+  
+  Population[PatientZero,"I"] <- 1
+  Population[PatientZero,"TimeStep"] <- t
+  
+  (Matrix <- matrix(0, ncol = NID, nrow = NID))
+  
+  Connections <- sample(1:(NID*NID), NID*NID/20)
+  
+  Matrix[Connections] <- 1
+  
+  Matrix[PatientZero,]
+  
+  t <- 1
+  
+  ToInfect <- rbinom(NID, 1, TransmissionProbability)
+  
+  ToInfect <- ToInfect*Matrix[PatientZero,]
+  
+  WhichToInfect <- which(ToInfect == 1)
+  
+  Population[WhichToInfect, "I"] <- 1
+  Population[WhichToInfect, "TimeStep"] <- t
+  
+  # Population %>% filter(TimeStep == 0)
+  
+  for(t in 2:100){
+    
+    print(t)
+    
+    Infected <- which(Population$I == 1)
+    
+    Matrix[Infected,]
+    
+    if(length(Infected) < 2){
+      
+      ToInfect <- Matrix[Infected,]
+      
+    }else{
+      
+      ToInfect <- Matrix[Infected,] %>% colSums
+      
+    }
+    
+    ToInfect[ToInfect>1] <- 1
+    
+    ToInfect <- ToInfect*rbinom(NID, 1, TransmissionProbability)
+    
+    WhichToInfect <- which(ToInfect == 1)
+    
+    WhichToInfect <- WhichToInfect %>% setdiff(which(Population$I == 1))
+    
+    Population[WhichToInfect, "I"] <- 1
+    Population[WhichToInfect, "TimeStep"] <- t
+    
+  }
+  
+  # i <- 1
+  
+  # Population %>% saveRDS("FritzePopulation1.rds")
+  # Population %>% saveRDS(paste0("FritzePopulation", i, ".rds"))
+  Population %>% saveRDS(paste0("Outputs/FritzePopulation", "_S_I_", TransmissionProbability, "_", i, ".rds"))
   
 }
 
-#i<- 1
+Population %>%
+  arrange(TimeStep) %>% 
+  mutate_at("TimeStep", ~replace_na(.x, 100)) %>% 
+  ggplot(aes(TimeStep, I)) + 
+  geom_point()
 
-#Population  %>%  saveRDS("Population1.rds")
-Population  %>%  saveRDS(paste("Population1", i, ".rds", sep=""))
+Population %<>%
+  arrange(TimeStep) %>% 
+  mutate_at("TimeStep", ~replace_na(.x, 100)) %>% 
+  mutate(N = 1:n())
 
-}
+Population %>% 
+  ggplot(aes(TimeStep, N)) + 
+  geom_line()
 
-Matrix
+library(magrittr)
+
+Population %<>% 
+  mutate_at("N", ~ifelse(I == 0, 97, .x))
+
+Population %>% 
+  ggplot(aes(TimeStep, N)) + 
+  geom_point(aes(colour = as.factor(I))) + 
+  scale_x_continuous(limits = c(0, 100))
